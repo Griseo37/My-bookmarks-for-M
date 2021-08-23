3d59b83a0926889443aaf64f79051ee5e8796802
Griseo37 <54672637+Griseo37@users.noreply.github.com>
2021-08-23 15:45:00 +0200

    Update local.py

--- bookmarks/settings/local.py
+++ bookmarks/settings/local.py
@@ -1,10 +1,12 @@
 from .base import *
+from os.path import dirname, abspath
+ROOT = dirname(abspath(__file__)).replace('\\', '/') + '/'
 
 DEBUG = False
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': ROOT + 'db.sqlite3', 
     }
 }
